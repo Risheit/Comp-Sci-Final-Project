32890f6ce04ba5bcee069462a77651b2a9bb286a
Risheit Munshi <21munshri@lrsd.net>
2021-06-03 14:45:47 -0500

    Added card class, It's empty right now, though.

---
+++ Comp Sci Final Project/Card.cs
@@ -0,0 +1,14 @@
+﻿// Card - This class contains all the info about a card and functions to modify them.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp_Sci_Final_Project
+{
+    class Card
+    {
+    }
+}
