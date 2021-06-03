74608b26135e1c274b2015daf05fd913e1a580b6
Risheit Munshi <21munshri@lrsd.net>
2021-06-02 19:32:54 -0500

    Started working on visual elements for form

--- Comp Sci Final Project/Form1.Designer.cs
+++ Comp Sci Final Project/Form1.Designer.cs
@@ -29,10 +29,18 @@ namespace Comp_Sci_Final_Project
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
